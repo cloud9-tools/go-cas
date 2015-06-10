d63f6464110a82ca53dac9b5f1b8583b625c38cf
Donald King <chronos@chronos-tachyon.net>
2015-06-10 10:27:28 -0700

    Add ls -0 flag.

--- libcasutil/ls.go
+++ libcasutil/ls.go
@@ -10,18 +10,20 @@ import (
 	"golang.org/x/net/context"
 )
 
-const LsHelpText = `Usage: casutil ls
+const LsHelpText = `Usage: casutil ls [-0]
 	Lists all CAS blocks.
 `
 
 type LsFlags struct {
 	Backend string
+	Zero bool
 }
 
 func LsAddFlags(fs *flag.FlagSet) interface{} {
 	f := &LsFlags{}
 	fs.StringVar(&f.Backend, "backend", "", "CAS backend to connect to")
 	fs.StringVar(&f.Backend, "B", "", "alias for --backend")
+	fs.BoolVar(&f.Zero, "0", false, "separate items with '\\0' instead of '\\n'")
 	return f
 }
 
@@ -53,6 +55,10 @@ func LsCmd(d *Dispatcher, ctx context.Context, args []string, fval interface{}) 
 		fmt.Fprintf(d.Err, "error: %v\n", err)
 		return 1
 	}
+	eol := "\n"
+	if f.Zero {
+		eol = "\x00"
+	}
 	for {
 		item, err := stream.Recv()
 		if err == io.EOF {
@@ -62,7 +68,7 @@ func LsCmd(d *Dispatcher, ctx context.Context, args []string, fval interface{}) 
 			fmt.Fprintf(d.Err, "error: %v\n", err)
 			return 1
 		}
-		fmt.Fprintf(d.Out, "%s\n", item.Addr)
+		fmt.Fprintf(d.Out, "%s%s", item.Addr, eol)
 	}
 	return 0
 }
