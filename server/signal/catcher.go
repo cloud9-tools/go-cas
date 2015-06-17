e9306120c6a550f0d747c67f768070dc48860212
Donald King <chronos@chronos-tachyon.net>
2015-06-16 23:10:59 -0700

    Add a signal-catching hook.

---
+++ server/signal/catcher.go
@@ -0,0 +1,45 @@
+package signal // import "github.com/chronos-tachyon/go-cas/server/signal"
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var IgnoreSignals = []os.Signal{
+	syscall.SIGHUP,
+}
+var ShutdownSignals = []os.Signal{
+	syscall.SIGINT,
+	syscall.SIGTERM,
+}
+
+type Catcher struct {
+	ch chan<- os.Signal
+}
+
+func Catch(signals []os.Signal, fn func()) *Catcher {
+	ch := make(chan os.Signal)
+	sc := &Catcher{ch}
+	go func() {
+		for {
+			select {
+			case sig := <-ch:
+				if sig == nil {
+					return
+				}
+				if fn != nil {
+					fn()
+				}
+			}
+		}
+	}()
+	signal.Notify(sc.ch, signals...)
+	return sc
+}
+
+func (sc *Catcher) Close() error {
+	signal.Stop(sc.ch)
+	close(sc.ch)
+	return nil
+}
