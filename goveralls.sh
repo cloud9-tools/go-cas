4e226c276f826ade8883259e362f8d72fe2865b1
Donald King <chronos@chronos-tachyon.net>
2015-06-20 17:47:35 -0700

    Try to fix coveralls.io support.  If at first you don't succeed...

--- goveralls.sh
+++ goveralls.sh
@@ -3,5 +3,5 @@ set -eu -o pipefail
 export PATH="${HOME}/gopath/bin:${PATH}"
 go list ./... | ( while read pkg; do
   gocov test "$@" "$pkg" || exit 1
-done ) | gocov-merge >coverage.json
-goveralls -service=travis-ci -gocovjson=coverage.json
+done ) | gocov-merge >gocov.json
+goveralls -service=travis-ci -gocovdata=gocov.json
